e2f94b13d6335022796703c667ac54cc99f46244
jayden <jiandongsong@126.com>
2018-05-24 11:52:38 +0800

    使用 c3p0 连接池获取连接

--- src/main/java/com/soong/jdbc/JdbcUtils.java
+++ src/main/java/com/soong/jdbc/JdbcUtils.java
@@ -1,5 +1,8 @@
 package com.soong.jdbc;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
@@ -9,6 +12,7 @@ import java.util.Properties;
 
 public class JdbcUtils {
     private static Properties props;
+    private static ComboPooledDataSource dataSource = new ComboPooledDataSource();
 
     // 静态代码块完成配置文件的加载和驱动的加载。这里使用静态代码块可以保证只加载一次
     static {
@@ -30,12 +34,33 @@ public class JdbcUtils {
     }
 
     /**
-     * 获取数据库连接
+     * 使用原始的方式获取数据库连接
+     * 需要提供四大连接参数
      */
-    public static Connection getConnection() throws SQLException {
+    @Deprecated
+    public static Connection getConnection_deprecated() throws SQLException {
         return DriverManager.getConnection(
                 props.getProperty("url"),
                 props.getProperty("username"),
                 props.getProperty("password"));
     }
+
+    /**
+     * 使用 c3p0 的方式获取连接，需要提供 c3p0-config.xml
+     */
+    public static Connection getConnection() {
+        try {
+            return dataSource.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 使用 c3p0 的方式获取 DataSource，需要提供 c3p0-config.xml
+     */
+    public static DataSource getDataSource() {
+        return dataSource;
+    }
+
 }
