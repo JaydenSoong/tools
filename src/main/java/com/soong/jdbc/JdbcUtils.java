c2c9c0fa2f101661444c20d68ec678b95e9b9b4a
jayden <jiandongsong@126.com>
2018-05-24 14:36:05 +0800

    修改方法名

--- src/main/java/com/soong/jdbc/JdbcUtils.java
+++ src/main/java/com/soong/jdbc/JdbcUtils.java
@@ -15,6 +15,7 @@ public class JdbcUtils {
     private static ComboPooledDataSource dataSource = new ComboPooledDataSource();
 
     // 静态代码块完成配置文件的加载和驱动的加载。这里使用静态代码块可以保证只加载一次
+    // 废弃方法 getConnectionNoPool 使用
     static {
         // 对 props 进行初始化，即加载配置文件
         try {
@@ -38,7 +39,7 @@ public class JdbcUtils {
      * 需要提供四大连接参数
      */
     @Deprecated
-    public static Connection getConnection_deprecated() throws SQLException {
+    public static Connection getConnectionNoPool() throws SQLException {
         return DriverManager.getConnection(
                 props.getProperty("url"),
                 props.getProperty("username"),
