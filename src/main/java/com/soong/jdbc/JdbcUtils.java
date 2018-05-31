8e4652342f3455f58e1e5734562abbdf3f19368a
Jayden <jiandongsong@126.com>
2018-05-31 16:02:46 +0800

    删除废弃方法

--- src/main/java/com/soong/jdbc/JdbcUtils.java
+++ src/main/java/com/soong/jdbc/JdbcUtils.java
@@ -3,10 +3,7 @@ package com.soong.jdbc;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 import javax.sql.DataSource;
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
@@ -16,38 +13,6 @@ public class JdbcUtils {
 
     // 支持多线程和事务的专用连接
     private static ThreadLocal<Connection> tl = new ThreadLocal<>();
-
-    // 静态代码块完成配置文件的加载和驱动的加载。这里使用静态代码块可以保证只加载一次
-    // 废弃方法 getConnectionNoPool 使用
-    static {
-        // 对 props 进行初始化，即加载配置文件
-        try {
-            InputStream in = JdbcUtils.class.getClassLoader().getResourceAsStream("dbconfig.properties");
-            props = new Properties();
-            props.load(in);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        // 加载驱动类
-        try {
-            Class.forName(props.getProperty("driver"));
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    /**
-     * 使用原始的方式获取数据库连接
-     * 需要提供四大连接参数
-     */
-    @Deprecated
-    public static Connection getConnectionNoPool() throws SQLException {
-        return DriverManager.getConnection(
-                props.getProperty("url"),
-                props.getProperty("username"),
-                props.getProperty("password"));
-    }
 
     /**
      * 使用 c3p0 的方式获取连接，需要提供 c3p0-config.xml
