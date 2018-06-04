8b15a953b9b78e7e15142f6eda94739026c61ccd
Jayden <jiandongsong@126.com>
2018-06-04 16:01:03 +0800

    添加 url 属性

--- src/main/java/com/soong/page/Page.java
+++ src/main/java/com/soong/page/Page.java
@@ -7,6 +7,20 @@ import java.util.List;
  * 使用该类时，用户需提供
  *  1. 每页记录数 ps(Page size)
  *  2. 需要显示的页面数量 sp(Show pages)
+ * 最终生成 Page 之前，用户还要设置 url，用户可通过在 Servlet 中定义如下方法获取到 URL 字符串，再将其设置成
+ * Page 对象的 URL
+ *
+ *  private String getUrl(HttpServletRequest request) {
+ *         String contextPath = request.getContextPath();
+ *         String servletPath = request.getServletPath();
+ *         String queryString = request.getQueryString();
+ *
+ *         if (queryString.contains("&pc=")) {
+ *             int index = queryString.indexOf("&pc=");
+ *             queryString = queryString.substring(0, index);
+ *         }
+ *         return contextPath + servletPath + "?" + queryString;
+ *     }
  */
 public class Page<T> {
     private int pc; // 当前页码 page code
@@ -17,7 +31,17 @@ public class Page<T> {
     // 确定页码
     private int sp; // 需要显示的页数 show pages; 该属性为业务属性，由用户最终确定
 
-    // 确定开位置
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    private String url;
+
+    // 确定页面开始位置
     public int getBegin() {
         // 当总页数小于等于需要显示的页数时，这时，指定 begin 为 1.
         if (getTp() <= sp) return 1;
@@ -36,7 +60,7 @@ public class Page<T> {
         return begin;
     }
 
-    // 确定结束位置
+    // 确定页码结束位置
     public int getEnd () {
         // 当总页数小于等于需要显示的页数时，这时，指定 end 为 tp.
         if (getTp() <= sp) return getTp();
