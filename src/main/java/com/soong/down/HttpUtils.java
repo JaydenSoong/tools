31236f573501dceeb5c0db5950e3e8ecad121f8d
Jayden <jiandongsong@126.com>
2018-06-10 10:37:28 +0800

    文件下载相关类

---
+++ src/main/java/com/soong/down/HttpUtils.java
@@ -0,0 +1,16 @@
+package com.soong.down;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class HttpUtils {
+    private static String[] IEBrowserSignals = {"MSIE", "Trident", "Edge"};
+
+    public static boolean isMSBrowser(HttpServletRequest request) {
+        String userAgent = request.getHeader("User-Agent");
+        for (String signal : IEBrowserSignals) {
+            if (userAgent.contains(signal))
+                return true;
+        }
+        return false;
+    }
+}
